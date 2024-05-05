fc67350d7ce835bbaf91614a4d301ae9839e2be0
Jingyi Xiang <jingyixj@gmail.com>
2024-05-05 16:44:08 +0200

    Mostrar en una tarjeta

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,3 @@
-// Aquí puedes añadir tu JavaScript
-// Recuerda reemplazar 'yourPublicKey' y 'yourPrivateKey' con tus claves reales
 let publicKey = '21dbc6b1dfb8c0244e3a1f05faa5ecc3';
 let privateKey = '7047e6cbbcd87df945706d8096b84c57ce883ce3';
 let characterId = '1009368'; // ID de Iron Man para prueba
