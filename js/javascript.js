89cafaf0a22f9ca5017917507065a6e722ce96f0
Jingyi Xiang <jingyixj@gmail.com>
2024-05-08 11:56:13 +0200

    muestra comics, alista los creadores del comic

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,19 @@
-let publicKey = '21dbc6b1dfb8c0244e3a1f05faa5ecc3';
-let privateKey = '7047e6cbbcd87df945706d8096b84c57ce883ce3';
-let characterId = '1009368'; // ID de Iron Man para prueba
+let clavePublica = '21dbc6b1dfb8c0244e3a1f05faa5ecc3';
+let clavePrivada = '7047e6cbbcd87df945706d8096b84c57ce883ce3';
+let idPersonaje = '1009368'; // ID de Iron Man para prueba
 
-let url = `https://gateway.marvel.com:443/v1/public/characters/${characterId}?apikey=${publicKey}`;
+let url = `https://gateway.marvel.com:443/v1/public/characters/${idPersonaje}?apikey=${clavePublica}`;
 
 fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        let character = data.data.results[0];
-        document.getElementById('character-info').innerHTML = `
-            <h1>${character.name}</h1>
-            <img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}">
-            <p>${character.description}</p>
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        let personaje = datos.data.results[0];
+        let contenedorPersonajes = document.getElementById('info-personaje');
+        contenedorPersonajes.innerHTML += `
+            <div class="tarjeta">
+                <h1>${personaje.name}</h1>
+                <img src="${personaje.thumbnail.path}.${personaje.thumbnail.extension}" alt="${personaje.name}">
+                <p>${personaje.description}</p>
+            </div>
         `;
     });
