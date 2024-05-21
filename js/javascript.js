11f6a30ae58d76b2bdd804d382367d4b250a41f7
Jingyi Xiang <jingyixj@gmail.com>
2024-05-21 21:03:51 +0200

    Cambios en el css y html para que se muestre comics y series en paralelo , y todo javascript en funciones, no funciona las series

--- js/javascript.js
+++ js/javascript.js
@@ -1,50 +1,39 @@
-let clavePublica = '21dbc6b1dfb8c0244e3a1f05faa5ecc3';
-let clavePrivada = '7047e6cbbcd87df945706d8096b84c57ce883ce3';
+const clavePublica = '21dbc6b1dfb8c0244e3a1f05faa5ecc3';
+const urlBase = 'https://gateway.marvel.com:443/v1/public/';
+const urlPersonajes = `${urlBase}characters?name=`;
+const urlSeriesBase = `${urlBase}series?characters=`;
 
 document.getElementById('boton-buscar').addEventListener('click', function() {
     let nombrePersonaje = document.getElementById('nombre-personaje').value;
-    let url = `https://gateway.marvel.com:443/v1/public/characters?name=${nombrePersonaje}&apikey=${clavePublica}`;
+    buscarPersonaje(nombrePersonaje);
+    buscarSeries(nombrePersonaje);
+});
 
-    fetch(url)
+function buscarPersonaje(nombre) {
+    const urlPersonaje = `${urlPersonajes}${nombre}&apikey=${clavePublica}`;
+
+    fetch(urlPersonaje)
         .then(respuesta => respuesta.json())
         .then(datos => {
+            let contenedorPersonajes = document.getElementById('info-personaje');
             if (datos.data.results.length > 0) {
                 let personaje = datos.data.results[0];
-                let contenedorPersonajes = document.getElementById('info-personaje');
-                contenedorPersonajes.innerHTML = `
-                    <div class="tarjeta">
-                        <h1>${personaje.name}</h1>
-                        <img src="${personaje.thumbnail.path}.${personaje.thumbnail.extension}" alt="${personaje.name}">
-                        <p>${personaje.description}</p>
-                    </div>
-                `;
-
-                // Obtén los cómics del personaje
-                let urlComics = personaje.comics.collectionURI.replace('http://', 'https://') + `?apikey=${clavePublica}`;
-                fetch(urlComics)
-                    .then(respuesta => respuesta.json())
-                    .then(datos => {
-                        let comics = datos.data.results;
-                        let contenedorComics = document.getElementById('contenedor-comics');
-                        contenedorComics.innerHTML = '';
-                        comics.forEach(comic => {
-                            let tarjetaComic = document.createElement('div');
-                            tarjetaComic.className = 'tarjeta-comic';
-                            tarjetaComic.innerHTML = `
-                                <h2>${comic.title}</h2>
-                                <ul></ul>
-                            `;
-                            let listaCreadores = tarjetaComic.querySelector('ul');
-                            comic.creators.items.forEach(creador => {
-                                let elementoLista = document.createElement('li');
-                                elementoLista.textContent = `${creador.name} (${creador.role})`;
-                                listaCreadores.appendChild(elementoLista);
-                            });
-                            contenedorComics.appendChild(tarjetaComic);
-                        });
-                    });
+                mostrarPersonaje(personaje);
+                buscarComics(personaje.comics.collectionURI);
             } else {
                 contenedorPersonajes.innerHTML = `<p>Personaje no encontrado</p>`;
             }
         });
-});
+}
+
+function mostrarPersonaje(personaje) {
+    let contenedorPersonajes = document.getElementById('info-personaje');
+    contenedorPersonajes.innerHTML = `
+        <div class="tarjeta">
+            <h1>${personaje.name}</h1>
+            <img src="${personaje.thumbnail.path}.${personaje.thumbnail.extension}" alt="${personaje.name}">
+            <p>${personaje.description}</p>
+        </div>
+    `;
+}
+
